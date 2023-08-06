1c251f25a39af67319befc49ded36e8c41f6b277
thisisazadi <a.msaboina@gmail.com>
2023-08-06 16:58:34 +0200

    update

--- script.js
+++ script.js
@@ -1,22 +1,71 @@
-const slides = document.querySelector("section div.slides")
+const slides = document.querySelectorAll("section div.slides")
 
-let current = 0
+slides.forEach(slide => {
+
+    let current = 0
 let z = 1000000000
 
-const images = slides.querySelectorAll("img")
+const images = slide.querySelectorAll("img")
 
 images.forEach(image => {
     z = z - 1
     image.style.zIndex = z
 })
 
-slides.addEventListener("click", function() {
+const timeline = gsap.timeline()
+
+timeline
+    .set(images, { 
+        x: () => {
+            return 500 * Math.random() - 250
+        },
+        y: "500%",
+        rotation: () => {
+            return 90 * Math.random() - 45
+        }
+    })
+    .to(images, { x: 0, y: 0, stagger: -0.25 })
+    .to(images, { 
+        rotation: () => {
+            return 16 * Math.random()
+        } 
+    })
+
+slide.addEventListener("click", function() {
     z = z - 1
+
+    let direction = "150%"
+    let midAngle = 15
+
+    if (Math.random() > 0.5) {
+        direction = "150%"
+        midAngle = -15
+    }
     
+    const currentImage = images[current]
+
+    const flipTimeline = gsap.timeline()
+
+    flipTimeline
+        .set(currentImage, { x: 0 })
+        .to(currentImage, { 
+            x: direction,
+            rotation: midAngle
+        })
+        .set(currentImage, { zIndex: z })
+        .to(currentImage, { 
+            x: 0,
+            rotation: () => {
+                return 16 * Math.random() - 8
+            }
+        })
+
     
-    images[current].style.zIndex = z
 
     current = current + 1
     current = current % images.length
 
 })
+
+})
+
